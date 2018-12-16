32966a7b5fa143b4c6363764c69a67676b30f637
HaGuesto <h.guertner@humanilog.org>
2018-12-16 17:09:32 +0100

    Installable Manifest

--- partner_family/__manifest__.py
+++ partner_family/__manifest__.py
@@ -2,37 +2,25 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Module name",
-    "summary": "Module summary",
+    "name": "Partner Family",
+    "summary": "",
     "version": "11.0.1.0.0",
     # see https://odoo-community.org/page/development-status
-    "development_status": "Alpha|Beta|Production/Stable|Mature",
+    "development_status": "Alpha",
     "category": "Uncategorized",
-    "website": "https://github.com/OCA/<repo>" or "https://github.com/OCA/<repo>/tree/<branch>/<addon>",
-    "author": "<AUTHOR(S)>, Odoo Community Association (OCA)",
+    "website": "https://github.com/humanilog/partner-contact/tree/11.0_partner_family/partner_family",
+    "author": "Ben Brich (humanilog), Odoo Community Association (OCA)",
     # see https://odoo-community.org/page/maintainer-role for a description of the maintainer role and responsibilities
-    "maintainers": ["your-github-login"],
+    "maintainers": ["HaGuesto"],
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "pre_init_hook": "pre_init_hook",
-    "post_init_hook": "post_init_hook",
-    "post_load": "post_load",
-    "uninstall_hook": "uninstall_hook",
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
+
     "depends": [
         "base",
     ],
     "data": [
+        "views/res_partner.xml"
     ],
-    "demo": [
-        "demo/assets.xml",
-        "demo/res_partner_demo.xml",
-    ],
-    "qweb": [
-        "static/src/xml/module_name.xml",
-    ]
+
 }
